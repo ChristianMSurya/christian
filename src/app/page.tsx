925886b2139ec4c502e89a8b42f23034acb79e5f
ChristianMSurya <cmatthew0705@gmail.com>
2024-12-07 21:21:26 -0500

    build error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,4 @@
-'use client';
-import { useState, useEffect, useRef } from "react";
-import { ChevronDown, Plus } from "lucide-react";
-
 export default function Home() {
-  const [showArrow, setShowArrow] = useState(false);
-  const secondSectionRef = useRef<HTMLDivElement>(null);
-  const thirdSectionRef = useRef<HTMLDivElement>(null);
-  const fourthSectionRef = useRef<HTMLDivElement>(null);
-
-  const handleHover = () => {
-    setTimeout(() => {
-      setShowArrow(true);
-    }, 2000);
-  };
 
   return (
     <div className="flex flex-col w-full min-h-screen scroll-smooth items-center justify-center font-[family-name:var(--font-geist-sans)]">
@@ -21,18 +7,18 @@ export default function Home() {
           <div className="flex flex-row w-full justify-center items-center gap-1">
             <div id="first-name" className="flex flex-row w-full items-center justify-end py-2 gap-1">
               <p id="friends" className="opacity-0 transition-opacity duration-300">my friends call me</p>
-              <p id="christian" onMouseEnter={handleHover} className="cursor-pointer transition-opacity duration-300">christian</p>
+              <p id="christian" className="cursor-pointer transition-opacity duration-300">christian</p>
             </div>
             <div id="second-name" className="flex flex-row w-full items-center justify-start py-2 gap-1">
-              <p id="matthew" onMouseEnter={handleHover} className="cursor-pointer transition-opacity duration-300">matthew</p>
+              <p id="matthew" className="cursor-pointer transition-opacity duration-300">matthew</p>
               <p id="family" className="opacity-0 transition-opacity duration-300">is what my family calls me</p>
             </div>
           </div>
-          <div id="down-arrow" 
+          {/* <div id="down-arrow" 
             className={`flex items-center justify-center transition-opacity duration-300 h-16 ${showArrow ? 'opacity-100' : 'opacity-0'}`}
           >
-            {/* <ChevronDown size={24} /> */}
-          </div>
+            <ChevronDown size={24} />
+          </div> */}
         </section>
         {/* <section ref={secondSectionRef} className="flex flex-col items-center justify-center min-h-screen w-full">
           <h2>Second Section</h2>
