008ba9ae170f1d3e519da71d42fbd500bdbd9ff0
ChristianMSurya <cmatthew0705@gmail.com>
2024-12-31 03:04:33 -0600

    made nav bar easier to click

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,22 +7,22 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <div className="flex flex-row gap-12">
+    <div className="flex flex-row gap-10">
       <Link 
         href="/" 
-        className={`cursor-pointer text-sm ${pathname === '/' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
+        className={`cursor-pointer text-sm py-2 pr-2 ${pathname === '/' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
       >
         <p>1</p>
       </Link>
       <Link 
         href="/work" 
-        className={`cursor-pointer text-sm ${pathname === '/work' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
+        className={`cursor-pointer text-sm p-2 ${pathname === '/work' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
       >
         <p>2</p>
       </Link>
       <Link 
         href="/writing" 
-        className={`cursor-pointer text-sm ${pathname === '/writing' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
+        className={`cursor-pointer text-sm p-2 ${pathname === '/writing' ? '' : 'opacity-40 hover:opacity-100 transition-opacity duration-300'}`}
       >
         <p>3</p>
       </Link>
